e2c1b727dfdcdb4d955fc85afd1a67f049d040f2
Brandon Macleod <BrandonMacleod21@gmail.com>
2020-07-27 18:45:20 +0000

    Add about page

--- app/controllers/about_controller.rb
+++ app/controllers/about_controller.rb
@@ -1,4 +1,4 @@
-class AboutController < ApplicationController 
-    def show
-    end
+class AboutController < ApplicationController
+  def show
   end
+end
--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,5 +1,7 @@
 class Admin::DashboardController < ApplicationController
   http_basic_authenticate_with name: ENV["USERNAME"], password: ENV["PASSWORD"]
   def show
+    @product_count = Product.order(id: :desc).all.size
+    @category_count = Category::count
   end
 end
