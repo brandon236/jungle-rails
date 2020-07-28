067b114d7e3182251a3d2e6e23823bfec1c9f26b
Brandon Macleod <BrandonMacleod21@gmail.com>
2020-07-28 00:20:22 +0000

    Fix product related issues

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -1,5 +1,4 @@
 class Admin::ProductsController < ApplicationController
-  http_basic_authenticate_with name: "Jungle", password: "book"
 
   def index
     @products = Product.order(id: :desc).all
@@ -8,7 +7,6 @@ class Admin::ProductsController < ApplicationController
   def new
     @product = Product.new
   end
-  http_basic_authenticate_with name: ENV["USERNAME"], password: ENV["PASSWORD"]
   def create
     @product = Product.new(product_params)
 
@@ -18,7 +16,6 @@ class Admin::ProductsController < ApplicationController
       render :new
     end
   end
-  http_basic_authenticate_with name: ENV["USERNAME"], password: ENV["PASSWORD"]
   def destroy
     @product = Product.find params[:id]
     @product.destroy
@@ -26,7 +23,6 @@ class Admin::ProductsController < ApplicationController
   end
 
   private
-  http_basic_authenticate_with name: ENV["USERNAME"], password: ENV["PASSWORD"]
 
   def product_params
     params.require(:product).permit(
