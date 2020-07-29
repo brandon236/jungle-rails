2ee274247c48489ce23601c39190c45a66686cc7
Brandon Macleod <BrandonMacleod21@gmail.com>
2020-07-29 16:35:17 +0000

    Create new specs

---
+++ spec/models/product_spec.rb
@@ -0,0 +1,56 @@
+require 'rails_helper'
+
+RSpec.describe Product, type: :model do
+  describe 'Validations' do
+    it 'should save succesfully' do
+      @category = Category.new
+      @product = Product.new;
+      @product.price_cents = 304032
+      @product.name = "new product"
+      @product.quantity = 15
+      @product.category = @category
+      @product.save!
+      expect(@product).to be_present
+    end
+    it 'should have a name' do
+      @category = Category.new
+      @product = Product.new;
+      @product.price_cents = 304032
+      @product.name = "new product"
+      @product.quantity = 15
+      @product.category = @category
+      @product.save!
+      expect(@product.name).to be_present
+    end
+    it 'should have a price' do
+      @category = Category.new
+      @product = Product.new;
+      @product.price_cents = 304032
+      @product.name = "new product"
+      @product.quantity = 15
+      @product.category = @category
+      @product.save!
+      expect(@product.price).to be_present
+    end
+    it 'should have a quantity' do
+      @category = Category.new
+      @product = Product.new;
+      @product.price_cents = 304032
+      @product.name = "new product"
+      @product.quantity = 15
+      @product.category = @category
+      @product.save!
+      expect(@product.quantity).to be_present
+    end
+    it 'should have a category' do
+      @category = Category.new
+      @product = Product.new;
+      @product.price_cents = 304032
+      @product.name = "new product"
+      @product.quantity = 15
+      @product.category = @category
+      @product.save!
+      expect(@product.category).to be_present
+    end
+  end
+end
