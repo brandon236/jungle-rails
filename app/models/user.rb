6325a69e8e72eece63837be982d82283bc754a8a
Brandon Macleod <BrandonMacleod21@gmail.com>
2020-07-30 08:18:44 +0000

    Add examples for edge cases

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,30 @@
 class User < ActiveRecord::Base
 
     has_secure_password
+    before_validation :downcase_email
+    before_save :downcase_email
+
+    def self.authenticate_with_credentials(email, password)
+        modifiedEmail = email.strip
+        user = User.find_by_email(modifiedEmail.downcase)
+        if (user && user.authenticate(password))
+          user
+        else
+          nil
+        end
+    end
+
 
     validates :email, :uniqueness => {:case_sensitive => false}
 
     validates :firstname, presence: true
     validates :lastname, presence: true
     validates :email, presence: true
-    validates :password, presence: true
-    validates :password_confirmation, presence: true
-    
+    validates :password, presence: true, :length => { :minimum => 5 }
+    validates :password_confirmation, presence: true, :length => { :minimum => 5 }
+
+    private
+      def downcase_email
+        self.email.downcase!
+      end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,26 +5,119 @@ RSpec.describe User, type: :model do
     it 'should have matching password and password_confirmation fields' do
       user = User.new
       user.firstname = "Brandon"
+      user.lastname = "Macleod"
+      user.email = "BrandonMacleod8@gmail.com"
       user.password = "password"
       user.password_confirmation = "password"
-      user.save
+      user.save!
       expect(user.password).to eq(user.password_confirmation)
     end
     it 'should not have the same email' do
       user = User.new
-      user.email = "BrandonMacleod21@gmail.com"
-      user.save
+      user.firstname = "Brandon"
+      user.lastname = "Macleod"
+      user.email = "BrandonMacleod8@gmail.com"
+      user.password = "password"
+      user.password_confirmation = "password"
+      user.save!
+      expect(user).to be_present
       user2 = User.new
-      user2.email = "BrandonMacleod21@gmail.com"
-      user2.save
-      expect(user2).to be_nil
+      user2.firstname = "Brandon"
+      user2.lastname = "Macleod"
+      user2.email = "BrandonMacleod8@gmail.com"
+      user2.password = "password"
+      user2.password_confirmation = "password"
+      user2.valid?
+      expect(user2).to_not be_valid
+      expect(user2.errors.full_messages).to include("Email has already been taken")
     end
     it 'should have a first name' do
       user = User.new
       user.firstname = "Brandon"
       user.lastname = "Macleod"
-      user.save
+      user.email = "BrandonMacleod8@gmail.com"
+      user.password = "password"
+      user.password_confirmation = "password"
+      user.save!
       expect(user.firstname).to be_present
+    end
+    it 'should have a last name' do
+      user = User.new
+      user.firstname = "Brandon"
+      user.lastname = "Macleod"
+      user.email = "BrandonMacleod8@gmail.com"
+      user.password = "password"
+      user.password_confirmation = "password"
+      user.save!
+      expect(user.lastname).to be_present
+    end
+    it 'should have an email' do
+      user = User.new
+      user.firstname = "Brandon"
+      user.lastname = "Macleod"
+      user.email = "BrandonMacleod8@gmail.com"
+      user.password = "password"
+      user.password_confirmation = "password"
+      user.save!
+      expect(user.email).to be_present
+    end
+    it 'should have a minimum password length' do
+      user = User.new
+      user.firstname = "Brandon"
+      user.lastname = "Macleod"
+      user.email = "BrandonMacleod8@gmail.com"
+      user.password = "yes"
+      user.password_confirmation = "yes"
+      user.valid?
+      expect(user).to_not be_valid
+      expect(user.errors.full_messages).to include("Password is too short (minimum is 5 characters)")
+    end
+  end
+
+  describe '.authenticate_with_credentials' do
+    it 'should return a valid user' do
+      user = User.new
+      user.firstname = "Brandon"
+      user.lastname = "Macleod"
+      user.email = "BrandonMacleod8@gmail.com"
+      user.password = "password"
+      user.password_confirmation = "password"
+      user.save!
+      newUser = User.authenticate_with_credentials(user.email, user.password)
+      expect(newUser).to_not be_nil
+    end
+    it 'should return a valid user if the email case is wrong' do
+      user = User.new
+      user.firstname = "Brandon"
+      user.lastname = "Macleod"
+      user.email = "BrandonMacleod8@gmail.com"
+      user.password = "password"
+      user.password_confirmation = "password"
+      user.save!
+      newUser = User.authenticate_with_credentials("brAndOnmAcLeoD8@gmail.com", user.password)
+      expect(newUser).to_not be_nil
+    end
+    it 'should return a valid user if the email contains spaces' do
+      user = User.new
+      user.firstname = "Brandon"
+      user.lastname = "Macleod"
+      user.email = "BrandonMacleod8@gmail.com"
+      user.password = "password"
+      user.password_confirmation = "password"
+      user.save!
+      newUser = User.authenticate_with_credentials(" BrandonMacleod8@gmail.com ", user.password)
+      expect(newUser).to_not be_nil
+    end
+    it 'should return a valid user if the email contains spaces and is in the wrong case' do
+      user = User.new
+      user.firstname = "Brandon"
+      user.lastname = "Macleod"
+      user.email = "BrandonMacleod8@gmail.com"
+      user.password = "password"
+      user.password_confirmation = "password"
+      user.save!
+      newUser = User.authenticate_with_credentials(" brAndOnmAcLEOd8@gmAil.com ", user.password)
+      expect(newUser).to_not be_nil
     end
   end
 end
